8674a14e0e8e7c409efaae8bc32a81789d614369
theshawe <david@invisiblebuilding.com>
2020-10-29 14:27:28 +0000

    website updates

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function() {
+  const offset = window.pageYOffset;
+  
+  if(offset > 75)
+    nav.classList.add('scroll')
+  else 
+    nav.classList.remove('scroll')
+});
+
+
